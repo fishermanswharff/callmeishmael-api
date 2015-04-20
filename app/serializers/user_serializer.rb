d3abffc30740774e25463c9dd88cf9fb85214020
fishermanswharff <fishermanswharff@github.com>
2015-04-20 14:23:07 -0400

    Autopush CMI API Master Branch

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :token, :firstname, :lastname, :username, :role, :email, :phonenumber, :active, :main_store_contact, :main_business_contact, :confirmed, :created_at, :updated_at
+
+  def created_at
+    object.created_at.strftime('%Y-%m-%d')
+  end
+  def updated_at
+    object.updated_at.strftime('%Y-%m-%d')
+  end
 end
