5609f3ee5a1d9e2d42624012eb0318e0b4588338
fishermanswharff <fishermanswharff@github.com>
2015-04-26 17:23:51 -0400

    venue specs all green

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,7 @@
 class VenuesController < ApplicationController
 
   before_action :set_venue, only: [:show, :update, :destroy]
+  before_filter :admin_only, only: [:create]
 
   def index
     venues = Venue.all
@@ -14,7 +15,7 @@ class VenuesController < ApplicationController
   def create
     @venue = Venue.create(venue_params)
     if @venue.save
-      render json: @venue, status: :created, location: venue_url
+      render json: @venue, status: :created, location: venue_url(@venue)
     else
       render json: @venue.errors, status: :unprocessable_entity
     end
@@ -34,11 +35,29 @@ class VenuesController < ApplicationController
   end
 
   private
+
   def venue_params
-    params.require(:venue).permit(:name, :status, :number_phones, :user)
+    params.require(:venue).permit(:name, :status, :number_phones, :user_id)
   end
 
   def set_venue
     @venue = Venue.find(params[:id])
+  end
+
+  def admin_only
+    unless is_admin?(get_token)
+      self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+      render json: {
+        error: 'You are not an admin'
+        }, status: 403
+    end
+  end
+
+  def get_token
+    request.headers.env['HTTP_AUTHORIZATION'].gsub(/Token token=/, "")
+  end
+
+  def is_admin?(token)
+    User.where(token: token)[0].admin?
   end
 end
--- spec/requests/venue_request_spec.rb
+++ spec/requests/venue_request_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe 'Venue API Endpoint' do
-  before(:each) do
+  before(:all) do
     Venue.destroy_all
     User.destroy_all
     @venue_admin = User.create({firstname: 'foo', lastname: 'bar', phonenumber: 5555555555, username: 'foobar', role: 'venue_admin', email: 'foo@bar.com', password: 'secret'})
+    @another_venue_admin = User.create({firstname: 'baz', lastname: 'fah', phonenumber: 5555555555, username: 'bazfah', role: 'venue_admin', email: 'baz@fah.com', password: 'secret'})
+    @admin = User.create({firstname: 'Logan', lastname: 'Smalley', phonenumber: 5555555555, username: 'logan', role: 'admin', email: 'logan@ted.com', password: 'secret'})
     @venues = Venue.create([
       { name: '9 Candlewick', user: @venue_admin, number_phones: 2 },
       { name: '21 Shepard', user: @venue_admin, number_phones: 1 },
@@ -44,4 +46,120 @@ describe 'Venue API Endpoint' do
       expect(venue[:user][:username]).to eq 'foobar'
     end
   end
+
+  describe '#create' do
+
+    context 'without an admin user’s authorization' do
+      before(:each) do
+        post '/venues',
+        { venue:
+          { name: 'Harvard Book Store', user_id: "#{@venue_admin.id}", number_phones: 1 }
+        }.to_json,
+        {
+          'Accept' => Mime::JSON,
+          'Content-Type' => Mime::JSON.to_s,
+          'HTTP_AUTHORIZATION' => "Token token=#{@venue_admin.token}"
+        }
+      end
+      it 'responds unauthorized' do
+        expect(response.status).to eq 403
+        r = json(response.body)
+        expect(r[:error]).to eq 'You are not an admin'
+      end
+    end
+
+    context 'with an admin user’s authorization' do
+      before(:each) do
+        post '/venues',
+        { venue:
+          { name: 'Harvard Book Store', user_id: @venue_admin.id, number_phones: 1 }
+        }.to_json,
+        {
+          'Accept' => Mime::JSON,
+          'Content-Type' => Mime::JSON.to_s,
+          'HTTP_AUTHORIZATION' => "Token token=#{@admin.token}"
+        }
+      end
+
+      it 'responds successfully' do
+        expect(response.status).to eq 201
+      end
+      it 'returns the newly created venue with the user' do
+        venue = json(response.body)
+        expect(venue[:user][:firstname]).to eq 'foo'
+      end
+    end
+  end
+
+  describe '#update' do
+    before(:each) do
+      patch "/venues/#{@venues[0].id}",
+      { venue:
+        {
+          name: '9 Candlewick',
+          number_phones: 3,
+          user_id: "#{@another_venue_admin.id}",
+        }
+      }.to_json,
+      {
+        'Accept' => Mime::JSON,
+        'Content-Type' => Mime::JSON.to_s,
+        'HTTP_AUTHORIZATION' => "Token token=#{@venue_admin.token}"
+      }
+    end
+    it 'responds successfully' do
+      expect(response.status).to eq 200
+    end
+    it 'updates only the changed attributes of the venue' do
+      venue = json(response.body)
+      expect(venue[:user][:id]).to eq "#{@another_venue_admin.id}"
+      expect(venue[:name]).to eq '9 Candlewick'
+      expect(venue[:number_phones]).to eq 3
+    end
+  end
+
+  describe '#destroy' do
+    before(:each) do
+      delete "/venues/#{@venues[0].id}"
+    end
+
+    it 'responds with no content' do
+      expect(response.body).to eq ''
+      expect(response.status).to eq 204
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
