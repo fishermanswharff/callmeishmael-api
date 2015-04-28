d8866b17b6bf60119f70830cabf9ef013436b647
fishermanswharff <fishermanswharff@github.com>
2015-04-27 22:02:25 -0400

    [ci skip] setting env to production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+ENV['RAILS_ENV'] ||= 'production'
 
 # Initialize the Rails application.
 Rails.application.initialize!
