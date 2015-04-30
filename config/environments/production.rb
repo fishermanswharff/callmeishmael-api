27802e125c805f386b433f701bc7761cd76a21c7
fishermanswharff <fishermanswharff@github.com>
2015-04-29 21:02:31 -0400

    adding deploy to heroku on travis build, updating production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.logger
-  config.action_mailer.default_url_options = { :host => 'http://callmeishmael.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://callmeishmael-api.herokuapp.com' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'admin@callmeishmael.com'}
