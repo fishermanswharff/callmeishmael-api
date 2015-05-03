49902ded262f95d670f5746048636f2c7938465b
fishermanswharff <fishermanswharff@github.com>
2015-05-03 09:55:28 -0400

    url sorting algorithm

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -16,9 +16,23 @@ class Phone < ActiveRecord::Base
   def get_urls
     # ordered by assignment: #,*,0,1,2,…
     stories = Story.find_by_sql("SELECT stories.url, stories.id, buttons.assignment FROM stories INNER JOIN buttons ON (buttons.story_id = stories.id) WHERE buttons.phone_id = #{self.id} ORDER BY buttons.assignment;")
-    buttons.map.with_index { |b,i| "#{b.assignment} with #{stories[i].url}" }.to_json
-    # binding.pry
-    # stories.to_json
+    hash = stories.each_with_object({}) { |i,o|
+      o[buttons.map { |b| b.assignment if b.story_id == i.id }.compact.first.to_s] = i.url
+    }
+    array = []
+    array[0] = hash['#']
+    array[1] = hash['*']
+    array[2] = hash['0']
+    array[3] = hash['1']
+    array[4] = hash['2']
+    array[5] = hash['3']
+    array[6] = hash['4']
+    array[7] = hash['5']
+    array[8] = hash['6']
+    array[9] = hash['7']
+    array[10] = hash['8']
+    array[11] = hash['9']
+    array.to_json
   end
 
   private
