669f3834c5ee1750bae3403bbac80c833a986f8e
fishermanswharff <fishermanswharff@github.com>
2015-05-22 00:07:12 -0400

    autopushing CMI API and skipping travis build [ci skip]

---
+++ lib/modules/FileRenamer.rb
@@ -0,0 +1,14 @@
+module FileRenamer
+
+  class Namer
+
+    def self.rename
+      filepath = '/Users/jason/Documents/thinq/Projects/Call-Me-Ishmael/Assets/Audio'
+
+      Dir.foreach(filepath){ |file|
+        next if file.in?(%w(. .. .DS_Store))
+        File.rename(filepath + '/' + file, filepath + '/' + file.gsub(/\s/,'-') + File.extname(file))
+      }
+    end
+  end
+end
