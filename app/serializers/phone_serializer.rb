f44f46c7214a9af910188c386c12e25b9df2f06a
fishermanswharff <fishermanswharff@github.com>
2015-07-25 11:58:58 -0400

    autopushing CMI API and skipping travis build [ci skip]

--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -12,9 +12,9 @@ class PhoneSerializer < ActiveModel::Serializer
   end
 
   def buttons
-    phone_stories = Story.connection.select_all("SELECT buttons.assignment, buttons.id, stories.id, stories.title, stories.created_at, stories.url FROM stories INNER JOIN buttons ON (buttons.story_id = stories.id) WHERE buttons.phone_id = #{object.id} ORDER BY buttons.assignment;").rows
+    phone_stories = Story.connection.select_all("SELECT buttons.assignment, buttons.id, stories.id, stories.title, stories.created_at, stories.url, stories.listens FROM stories INNER JOIN buttons ON (buttons.story_id = stories.id) WHERE buttons.phone_id = #{object.id} ORDER BY buttons.assignment;").rows
     stories = phone_stories.each_with_object({}) { |i,o|
-      o[i[0]] = { button_id: i[1], story_id: i[2], title: i[3], created_at: Date.parse(i[4]).strftime('%A, %D') , url: i[5] }
+      o[i[0]] = { button_id: i[1], story_id: i[2], title: i[3], created_at: Date.parse(i[4]).strftime('%A, %D') , url: i[5], listens: i[6] }
     }
     array = []
     array[0] = { '1' => stories['1'] }
