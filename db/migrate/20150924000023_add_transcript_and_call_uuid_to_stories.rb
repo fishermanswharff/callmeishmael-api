2573c637392c873bcafe7aad2490201620928963
fishermanswharff <fishermanswharff@github.com>
2015-10-19 19:47:11 -0400

    removing update_attributes call

--- db/migrate/20150924000023_add_transcript_and_call_uuid_to_stories.rb
+++ db/migrate/20150924000023_add_transcript_and_call_uuid_to_stories.rb
@@ -2,7 +2,5 @@ class AddTranscriptAndCallUuidToStories < ActiveRecord::Migration
   def change
     add_column :stories, :transcript_url, :string
     add_column :stories, :call_uuid, :integer
-    Story.all.update_attributes(transcript_url: 'http://www.google.com')
-    Story.all.update_attributes(call_uuid: '000')
   end
 end
