c6e00dd100ea7fb662848099a5e5b4242288bbb0
fishermanswharff <fishermanswharff@github.com>
2015-10-27 01:32:15 -0400

    autopushing CMI API and skipping travis build [ci skip]

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.1.0'
 set :application, 'callmeishmael_api'
 set :repo_url, 'git@github.com:fishermanswharff/callmeishmael-api.git'
 
-set :deploy_to, '/home/ubuntu/www/callmeishmael-api'
+set :deploy_to, '/home/ubuntu/www/callmeishmael_api'
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system shared}
 
@@ -40,7 +40,6 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :keep_releases, 5
 
 namespace :deploy do
-
 
   desc 'Restart application'
   task :restart do
