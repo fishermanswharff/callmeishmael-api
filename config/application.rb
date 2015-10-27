1e6ff934463e8b78094ed014fac4cd2723112e50
fishermanswharff <fishermanswharff@github.com>
2015-10-27 02:10:59 -0400

    autopushing CMI API and skipping travis build [ci skip]

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PhoneApi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib/modules')
-    config.middleware.use Rack::SslEnforcer, only_environments: ['production', /^QA/]
+    # config.middleware.use Rack::SslEnforcer, only_environments: ['production', /^QA/]
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
