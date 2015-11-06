1d9b0e4df1cf61963e1a3ed26fde899fc3a183ac
Jason Wharff <fishermanswharff@mac.com>
2015-11-05 20:25:32 -0500

    autopushing CMI API and skipping travis build [ci skip]

--- lib/phone_whisperer.rb
+++ lib/phone_whisperer.rb
@@ -1,7 +1,7 @@
 class PhoneWhisperer
 
   def initialize(host,name,password)
-    Net::SSH.start("localhost", "pi", password: 'raspberry', port: 2224) do |ssh|
+    Net::SSH.start(host, name, password: 'raspberry', port: 2224) do |ssh|
       ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py AudioPlayer.playContent 1"
     end
   end
@@ -15,7 +15,7 @@ where are the commands going to live?
 require 'rubygems'
 require 'net/ssh'
 Net::SSH.start("localhost", "pi", password: 'raspberry', port: 2224) do |ssh|
-  ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py AudioPlayer.playContent 1"
+  ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py AudioPlayer.playRingtone"
 end
 
 =end
