a0bea0f0eb8929131d541a5168b5dc918d9d8ffa
Jason Wharff <fishermanswharff@mac.com>
2015-11-08 11:52:23 -0500

    autopushing CMI API and skipping travis build [ci skip]

--- lib/phone_whisperer.rb
+++ lib/phone_whisperer.rb
@@ -4,7 +4,10 @@ class PhoneWhisperer
 
   def initialize
     # set these in environment variables.
-    Net::SSH.start('localhost', 'ubuntu', password: 'raspberry', port: 2224) do |ssh|
+    Net::SSH.start("localhost", "pi", password: 'raspberry', port: 2224) do |ssh|
+      ssh.open_channel do |ch|
+        ch.send_data("raspberry\n")
+      end
       ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py AudioPlayer.playRingtone"
     end
   end
@@ -19,7 +22,10 @@ phones_controller.
 require 'rubygems'
 require 'net/ssh'
 Net::SSH.start("localhost", "pi", password: 'raspberry', port: 2224) do |ssh|
-  ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py NetSync.syncFiles"
+  ssh.open_channel do |ch|
+    ch.send_data("raspberry\n")
+  end
+  ssh.exec! "sudo python /media/usb0/CMI-final/phone/remoteCommands.py AudioPlayer.playRingtone"
 end
 
 =end
